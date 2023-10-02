889911b6d6b1e52af18300538909d0cb0eb0e355
Erick Andrighetti <erickandrighetti.4062@aluno.saojudas.br>
2023-10-02 20:58:28 -0300

    app11(login): campos login e senha

---
+++ src/main/java/br/com/estranho/trade/Trade.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.estranho.trade;
+
+/**
+ *
+ * @author 823144062
+ */
+public class Trade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
