1edc535b39eac3bef298f1117cd07596a5a6144c
Erick <erickandrighetti08@hotmail.com>
2023-11-17 20:52:08 -0300

    maits teste merda

---
+++ src/main/java/br/com/mais trea.java
@@ -0,0 +1,9 @@
+main git push -f -u origin
+vax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jPasswordField1, javax.
